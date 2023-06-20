fb3150523b8a1ff18eae154b60ae489c3c6124d9
Iago Carvalho <iagocarvalhoweb@gmail.com>
2023-06-20 12:02:55 -0300

    config jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$'
+};
