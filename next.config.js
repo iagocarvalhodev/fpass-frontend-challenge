9d34489272c1cbee426e6aa6443b28c13c196ef0
Iago Carvalho <iagocarvalhoweb@gmail.com>
2023-06-21 15:45:21 -0300

    add config env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import("next").NextConfig} */
+require('dotenv').config();
+
 module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    PRIVATE_KEY: process.env.PRIVATE_KEY
+  },
   reactStrictMode: true,
   experimental: {
-    appDir: true
+    reactRoot: true
   }
 };
--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import md5 from 'md5';
 
 const time = Number(new Date());
-const publickey = '22b173d25530272f12058a2dcacb3dc8';
-const privateKey = '2ae5d5c7dd149c1927af5ef8c3f7ab2c395c0a30';
+const publickey = process.env.API_KEY as string;
+const privateKey = process.env.PRIVATE_KEY as string;
 
 export const axiosInstance = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public',
